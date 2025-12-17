f531a6d33b26806d2a476d8b94521c5b3d1f80e9
Abhay Katre <abhaykatre70@gmail.com>
2025-12-17 21:48:32 +0530

    Variables and Data types added

---
+++ javascript_basic/data_types.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js as newer version
+
+// alert(3+3) --- it is use when usig browser here we are uing nodejs 
+
+//premitive datatypes
+let num=56 //number 2 to pow 53 
+let nameCap='ABHAY' //string
+let nameSmall="abhay" //string
+let condition=true //boolean
+let check=false
+
+//null => standalone value 
+//undefined 
+//symbol =>use for finding uniqueness use in react
+
+//object
+console.log(typeof('Abhay'))  //string
+console.log(typeof num)  //num
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
---
+++ javascript_basic/variables.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="abhaykatre70@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+// accountId=2 not allowed 
+accountEmail="ab@gmail.com"
+accountPassword="11111"
+accountCity="bengaluru"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
